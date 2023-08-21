fa410b04d1ea77f0fcd22fc453c83e32be023ec1
vietchinh <1348151+vietchinh@users.noreply.github.com>
2023-08-22 01:53:44 +0200

    feat: added monthly auto upgrade with reboot

---
+++ scripts/ucore-systemd.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+cat > /usr/etc/systemd/system/rpm-ostreed-upgrade-reboot.service << EOF
+# workaround for missing reboot policy
+# https://github.com/coreos/rpm-ostree/issues/2843
+[Unit]
+Description=rpm-ostree upgrade and reboot
+ConditionPathExists=/run/ostree-booted
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/rpm-ostree upgrade --reboot
+#StandardOutput=null
+EOF
+
+cat > /usr/etc/systemd/system/rpm-ostreed-upgrade-reboot.timer << EOF
+[Unit]
+Description=rpm-ostree upgrade and reboot trigger on every first monday of the month
+ConditionPathExists=/run/ostree-booted
+
+[Timer]
+OnCalendar=Mon *-*-1..7 03:00:00
+Persistent=true
+
+[Install]
+WantedBy=timers.target
+EOF
+
+systemctl enable rpm-ostreed-upgrade-reboot.timer
