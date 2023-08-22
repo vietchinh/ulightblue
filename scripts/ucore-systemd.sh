a6fe7575ff3d6ba85963931fa387f360929477c7
vietchinh <1348151+vietchinh@users.noreply.github.com>
2023-08-22 02:18:59 +0200

    fix: use cat again but lib parent folder is usr

--- scripts/ucore-systemd.sh
+++ scripts/ucore-systemd.sh
@@ -6,5 +6,31 @@
 set -oue pipefail
 
 # Your code goes here.
+cat > /usr/lib/systemd/system/rpm-ostreed-upgrade-reboot.service << EOF
+# workaround for missing reboot policy
+# https://github.com/coreos/rpm-ostree/issues/2843
+[Unit]
+Description=rpm-ostree upgrade and reboot
+ConditionPathExists=/run/ostree-booted
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/rpm-ostree upgrade --reboot
+#StandardOutput=null
+EOF
+
+cat > /usr/lib/systemd/system/rpm-ostreed-upgrade-reboot.timer << EOF
+[Unit]
+Description=rpm-ostree upgrade and reboot trigger on every first monday of the month
+ConditionPathExists=/run/ostree-booted
+
+[Timer]
+OnCalendar=Mon *-*-1..7 03:00:00
+Persistent=true
+
+[Install]
+WantedBy=timers.target
+EOF
+
 systemctl enable rpm-ostreed-upgrade-reboot.timer
 systemctl disable rpm-ostreed-automatic.timer
