3b793a4702368477305cf0a53e7d5d7c572f770e
vietchinh <1348151+vietchinh@users.noreply.github.com>
2023-09-12 04:42:39 +0200

    feat: migrate from v0.x to v1.0 of the template
    (cherry picked from commit cf6a7a2a183d6cdb3840b516edc2df5e17d11b87)

---
+++ config/scripts/kinoite.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+rpm-ostree install --assumeyes --apply-live --force-replacefiles /tmp/ublue-os-update-services.noarch.rpm /tmp/ublue-os-udev-rules.noarch.rpm
+
+wget https://raw.githubusercontent.com/ublue-os/main/main/main-post-install.sh -P /tmp/config/scripts
+chmod +x /tmp/scripts/main-post-install.sh
