ef7dfbb8d072ccd5c72afe03b834846bde4d21a8
vietchinh <1348151+vietchinh@users.noreply.github.com>
2023-11-07 23:40:39 +0100

    fix: added patch + search for kernel-devel

--- config/scripts/vgpu.sh
+++ config/scripts/vgpu.sh
@@ -10,7 +10,7 @@ NVIDIA_VERSION="535.104"
 ZIP_NAME="NVIDIA-GRID-Linux-KVM-${NVIDIA_VERSION}.06-535.104.05-537.13.zip"
 
 rpm-ostree install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" "https://github.com/rpmsphere/noarch/raw/master/r/rpmsphere-release-$(rpm -E %fedora)-1.noarch.rpm"
-rpm-ostree install wget p7zip p7zip-plugins mscompress osslsigncode git kernel-devel unzip
+rpm-ostree install wget p7zip p7zip-plugins mscompress osslsigncode git kernel-devel unzip patch
 
 git clone --recursive https://github.com/VGPU-Community-Drivers/vGPU-Unlock-patcher.git -b ${NVIDIA_VERSION}
 
@@ -30,7 +30,7 @@ chmod +x patch.sh
 
 ./patch.sh general-merge
 
-KERNEL_VERSION="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+KERNEL_VERSION="$(rpm -q kernel-devel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
 ./NVIDIA-Linux-x86_64-535.104.05-merged-vgpu-kvm-patched/nvidia-installer -s --kernel-source-path /usr/src/kernels/"${KERNEL_VERSION}" --no-systemd --dkms
 
