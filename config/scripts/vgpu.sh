df1e0ff06c0921f0e14a2085d0cb4b95286257c4
vietchinh <1348151+vietchinh@users.noreply.github.com>
2023-11-08 00:04:34 +0100

    chore: added cleanup

--- config/scripts/vgpu.sh
+++ config/scripts/vgpu.sh
@@ -8,9 +8,10 @@ cd /tmp
 
 NVIDIA_VERSION="535.104"
 ZIP_NAME="NVIDIA-GRID-Linux-KVM-${NVIDIA_VERSION}.06-535.104.05-537.13.zip"
+DEVELOPMENT_PACKAGES="wget p7zip p7zip-plugins mscompress osslsigncode git kernel-devel unzip patch vulkan-loader"
 
 rpm-ostree install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" "https://github.com/rpmsphere/noarch/raw/master/r/rpmsphere-release-$(rpm -E %fedora)-1.noarch.rpm"
-rpm-ostree install wget p7zip p7zip-plugins mscompress osslsigncode git kernel-devel unzip patch
+rpm-ostree install "${DEVELOPMENT_PACKAGES}" vulkan-loader
 
 git clone --recursive https://github.com/VGPU-Community-Drivers/vGPU-Unlock-patcher.git -b ${NVIDIA_VERSION}
 
@@ -36,3 +37,4 @@ KERNEL_VERSION="$(rpm -q kernel-devel --queryformat '%{VERSION}-%{RELEASE}.%{ARC
 
 cd ..
 rm -rf ${VGPU_FOLDER}
+rpm-ostree remove "${DEVELOPMENT_PACKAGES}" "rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" "https://github.com/rpmsphere/noarch/raw/master/r/rpmsphere-release-$(rpm -E %fedora)-1.noarch.rpm"
