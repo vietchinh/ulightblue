f833ae60fb89b7d223241b818b4a2caa8facca82
vietchinh <1348151+vietchinh@users.noreply.github.com>
2023-11-10 00:32:33 +0100

    fix: attempt to run nvidia driver2

--- config/scripts/vgpu.sh
+++ config/scripts/vgpu.sh
@@ -9,7 +9,7 @@ cd /tmp
 NVIDIA_VERSION="535.104"
 ZIP_NAME="NVIDIA-GRID-Linux-KVM-${NVIDIA_VERSION}.06-535.104.05-537.13.zip"
 KERNEL_VERSION="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
-DEVELOPMENT_PACKAGES="wget p7zip p7zip-plugins mscompress osslsigncode git kernel-devel-${KERNEL_VERSION} unzip patch vulkan-loader"
+DEVELOPMENT_PACKAGES="wget p7zip p7zip-plugins mscompress osslsigncode git kernel-devel-${KERNEL_VERSION} unzip patch vulkan-loader akmods"
 
 rpm-ostree install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" "https://github.com/rpmsphere/noarch/raw/master/r/rpmsphere-release-$(rpm -E %fedora)-1.noarch.rpm"
 # shellcheck disable=SC2086
@@ -33,7 +33,8 @@ chmod +x patch.sh
 
 ./patch.sh general-merge
 
-./NVIDIA-Linux-x86_64-535.104.05-merged-vgpu-kvm-patched/nvidia-installer -s --kernel-source-path /usr/src/kernels/"${KERNEL_VERSION}" --no-systemd --no-dkms --driver
+./NVIDIA-Linux-x86_64-535.104.05-merged-vgpu-kvm-patched/nvidia-installer -s --kernel-source-path /usr/src/kernels/"${KERNEL_VERSION}" --no-systemd --no-dkms
+akmods --force --kernels "${KERNEL_VERSION}" --kmod "nvidia"
 
 cd ..
 rm -rf ${VGPU_FOLDER}
