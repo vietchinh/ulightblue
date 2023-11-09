36bc9d4ccd33a81bc2115588716458672af1d21d
vietchinh <1348151+vietchinh@users.noreply.github.com>
2023-11-09 23:26:52 +0100

    fix: use the kernel version as devel for vgpu

--- config/scripts/vgpu.sh
+++ config/scripts/vgpu.sh
@@ -8,7 +8,8 @@ cd /tmp
 
 NVIDIA_VERSION="535.104"
 ZIP_NAME="NVIDIA-GRID-Linux-KVM-${NVIDIA_VERSION}.06-535.104.05-537.13.zip"
-DEVELOPMENT_PACKAGES="wget p7zip p7zip-plugins mscompress osslsigncode git kernel-devel unzip patch vulkan-loader"
+KERNEL_VERSION="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+DEVELOPMENT_PACKAGES="wget p7zip p7zip-plugins mscompress osslsigncode git kernel-devel-${KERNEL_VERSION} unzip patch vulkan-loader"
 
 rpm-ostree install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" "https://github.com/rpmsphere/noarch/raw/master/r/rpmsphere-release-$(rpm -E %fedora)-1.noarch.rpm"
 # shellcheck disable=SC2086
@@ -31,8 +32,6 @@ mv Signing_Keys/* .
 chmod +x patch.sh
 
 ./patch.sh general-merge
-
-KERNEL_VERSION="$(rpm -q kernel-devel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
 ./NVIDIA-Linux-x86_64-535.104.05-merged-vgpu-kvm-patched/nvidia-installer -s --kernel-source-path /usr/src/kernels/"${KERNEL_VERSION}" --no-systemd --dkms
 
